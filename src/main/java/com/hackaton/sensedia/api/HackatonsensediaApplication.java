7b1c2deb1298ebc88f41b731deab3db9b5370b24
root <root@localhost.linuxpc>
2017-08-21 22:31:56 -0300

    Desenvolvimento do projeto hackaton sensedia e uma api teste já funcionando.

---
+++ src/main/java/com/hackaton/sensedia/api/HackatonsensediaApplication.java
@@ -0,0 +1,12 @@
+package com.hackaton.sensedia.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonsensediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackatonsensediaApplication.class, args);
+	}
+}
---
+++ src/main/java/com/hackaton/sensedia/api/repository/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package com.hackaton.sensedia.api.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.hackaton.sensedia.api.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+
+}
