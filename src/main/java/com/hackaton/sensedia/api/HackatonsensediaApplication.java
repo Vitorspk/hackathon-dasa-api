c942c7f1598f987c58307197fac0faedf8af0b32
vschiavo <vschiavo@localhost.vitorschiavo>
2017-09-16 19:25:47 -0300

    teste

--- src/main/java/com/hackaton/sensedia/api/HackatonsensediaApplication.java
+++ src/main/java/com/hackaton/sensedia/api/HackatonsensediaApplication.java
@@ -3,7 +3,7 @@ package com.hackaton.sensedia.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//teste
+
 @SpringBootApplication
 public class HackatonsensediaApplication {
 
